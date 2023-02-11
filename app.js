e249ca16f4c0a2ee9f93572abb6dc5a84cf0ba9b
Eduardo Soriano <EDuardDOsm.13@gmail.com>
2023-02-10 19:06:56 -0600

    Upload changes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-//const display=document.getElementById("display"); este es menos recomendable que la linea 2
 /* display */
+//const display=document.getElementById("display"); este es menos recomendable que la linea 3
 const display=document.querySelector("div#display");
 
 /* ac */
@@ -22,6 +22,10 @@ sig.addEventListener("click",()=>{
 
 /* percent */
 const percent=document.querySelector("div#percent");
+percent.addEventListener("click",()=>{
+    const currDisplay=display.innerHTML;
+    display.innerHTML=`${Number(currDisplay)/100}`;
+});
 
 /* dot */
 const dot=document.querySelector("div#dot");
@@ -48,5 +52,26 @@ const numberHandler=(n)=> {
     if(currDisplay==="0"){
         newDisplay=n;
     }
-    display.innerHTML=Number(newDisplay);
+    display.innerHTML=`${Number(newDisplay)}`;
 };
+
+/* operators */
+const div=document.querySelector("div#division");
+const mult=document.querySelector("div#mult");
+const sum=document.querySelector("div#sum");
+const sub=document.querySelector("div#sub");
+
+const operators=[
+    {elmnt: div,op:"/"},
+    {elmnt: mult,op:"*"},
+    {elmnt: sum,op:"+"},
+    {elmnt: sub,op:"-"}
+];
+operators.forEach((oper)=>oper.elmnt.addEventListener("click",()=>operationHandler(oper.op)));
+
+const operationHandler=(op)=>{
+    
+};
+
+/* equal */
+const equal=document.querySelector("div#equal");
